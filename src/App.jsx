712ec57059bf387eaad3f6ebb06a98611ea5f7dc
Khilesh <software@buildint.co>
2024-07-19 16:23:25 +0530

    createclient route

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { Navbar } from "./components/navbar/Navbar";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import Client from "./components/client/Client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Clients from "./components/client/Client";
 import ClientDetails from "./components/client/ClientDetails";
+import CreateClient from "./components/client/CreateClient"; // Import the CreateClient component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
   const bgColor = "#0F172B";
@@ -39,6 +39,15 @@ export default function App() {
               <>
                 <Navbar bgColor={bgColor} textColor={textColor} />
                 <ClientDetails />
+              </>
+            }
+          />
+          <Route
+            path="/create-client"
+            element={
+              <>
+                <Navbar bgColor={bgColor} textColor={textColor} />
+                <CreateClient /> {/* Add the CreateClient component */}
               </>
             }
           />
---
+++ src/components/client/CreateClient.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateClient = () => {
+  return (
+    
+    <div>
+      <h1>Create Client</h1>
+      {/* Your form or content for creating a client goes here */}
+    </div>
+  );
+};
+
+export default CreateClient;
