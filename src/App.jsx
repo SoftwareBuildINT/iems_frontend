c4c94e06e1f705c8a0d69f867bb8f4666dbd015b
Khilesh <software@buildint.co>
2024-07-19 12:54:14 +0530

    Client Management page + Client Details page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Navbar } from "./components/navbar/Navbar";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Clients from "./components/client/Client";
+import ClientDetails from "./components/client/ClientDetails";
 
 export default function App() {
   return (
@@ -15,6 +17,24 @@ export default function App() {
               <>
                 <Navbar bgColor="#0F172B" textColor="#ffffff" />
                 <Dashboard />
+              </>
+            }
+          />
+          <Route
+            path="/client"
+            element={
+              <>
+                <Navbar bgColor="#0F172B" textColor="#ffffff" />
+                <Clients />
+              </>
+            }
+          />
+          <Route
+            path="/client-details/:clientId"
+            element={
+              <>
+                <Navbar bgColor="#0F172B" textColor="#ffffff" />
+                <ClientDetails />
               </>
             }
           />
